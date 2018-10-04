100a37a540e77983a6abbde93f52f0db9c83e4ee
Paul Felt <paul.lewis.felt@gmail.com>
2018-10-03 21:59:18 -0600

    applied vflip to picamera

--- marion/picam_sensor.py
+++ marion/picam_sensor.py
@@ -1,45 +1,37 @@
 from io import BytesIO
 from time import sleep
 from picamera import PiCamera
-
 from geppetto_client import Sensor
 
 class PicamSensor(Sensor):
-    def __init__(self, host, port, robot_name, sensor_name, width=2592/16, height=1944/16):
+    def __init__(self, host, port, robot_name, sensor_name, width=2592/8, height=1944/8, vflip=True, hflip=False, **camargs):
         self.dims = width, height
-        self.image_gen = PiCamera().capture_continuous(BytesIO(), 'jpeg')
+        self.camargs = camargs
+        self.camargs['vflip'] = True
+        self.image_gen = None
         super(PicamSensor, self).__init__(host, port, robot_name, sensor_name)
+        sleep(3)
     def get_mediatype(self):
         return 'video'
     def get_reading(self):
-        print('getting video')
+        if self.image_gen is None:
+            print('initializing camera')
+            cam = PiCamera()
+            # apply user-supplied settings
+            for key,val in self.camargs.items():
+                setattr(cam, key, val)
+            self.image_gen = cam.capture_continuous(BytesIO(), 'jpeg', resize=self.dims, use_video_port=True)
+        #print('getting video')
         #return bytearray(b'1234567890')
-        pic_stream = BytesIO()
         stream = self.image_gen.next()
-        print('got video stream')
+        # read out bytes
         stream.seek(0)
         pic_bytes = bytearray(stream.read())
-        print('got video bytes')
+        # reset stream for next capture
+        stream.seek(0) 
         stream.truncate()
-        print('emitting video frame from: %s (%s)'%(self.get_socketio_name(),len(pic_bytes)))
-        #return bytearray(b'1234567890')
+        #print('emitting video frame from: %s (%s)'%(self.get_socketio_name(),len(pic_bytes)))
         return pic_bytes
     #def close(self):
     #    self.camera.close()
 
-#try:
-#    # Create an in-memory stream
-#    #my_stream = BytesIO()
-#    camera = PiCamera()
-#    #camera.start_preview()
-#    # Camera warm-up time
-#    sleep(2)
-#    while True:
-#        my_stream = open('/tmp/image.jpg', 'w')
-#        camera.capture(my_stream, 'jpeg', resize=(600,480))
-#        my_stream.close()
-#        sleep(1)
-#finally:
-#    my_stream.close()
-#    #camera.stop_preview()
-#    camera.close()
